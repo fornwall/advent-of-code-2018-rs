72a16248e57e24a2369060553fa52b8d19322ac0
Fredrik Fornwall <fredrik@fornwall.net>
2018-12-04 20:51:50 +0100

    Use hashmap in day 2 solution

--- src/day2.rs
+++ src/day2.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 pub fn solve() {
     let input_string = include_str!("day2_input.txt");
     let result_part1 = evaluate_part1(input_string);
@@ -7,51 +9,26 @@ pub fn solve() {
     println!("Part 2: {}", result_part2);
 }
 
-fn line_checksum(line: &[u8]) -> (i64, i64) {
-    let mut occurrences = (0i64, 0i64);
-    let mut current_char: i16 = -1;
-    let mut current_count = 0;
-
-    for c in line.iter() {
-        if *c == current_char as u8 {
-            current_count += 1;
-        } else {
-            match current_count {
-                2 => occurrences.0 = 1,
-                3 => occurrences.1 = 1,
-                _ => (),
-            }
-            current_char = i16::from(*c);
-            current_count = 1;
-        }
-    }
-
-    match current_count {
-        2 => occurrences.0 = 1,
-        3 => occurrences.1 = 1,
-        _ => (),
-    }
-
-    occurrences
-}
-
 fn evaluate_part1(input_string: &str) -> i64 {
-    let occurrences = input_string
-        .split_whitespace()
-        .map(|line| {
-            let mut res = line.as_bytes().to_owned();
-            res.sort();
-            res
-        }).fold((0, 0), |state, line| {
-            let line_occurrences = line_checksum(&line);
-            (state.0 + line_occurrences.0, state.1 + line_occurrences.1)
-        });
+    let picks = input_string.lines().fold((0, 0), |state, line| {
+        let mut occurrences = HashMap::new();
 
-    occurrences.0 * occurrences.1
+        line.chars()
+            .for_each(|c| *occurrences.entry(c).or_insert(0) += 1);
+
+        let has_occurence = |count| occurrences.iter().any(|(_key, &value)| value == count);
+
+        (
+            state.0 + has_occurence(2) as i64,
+            state.1 + has_occurence(3) as i64,
+        )
+    });
+
+    picks.0 * picks.1
 }
 
 fn evaluate_part2(input_string: &str) -> String {
-    let input: Vec<&str> = input_string.split_whitespace().collect();
+    let input: Vec<&str> = input_string.lines().collect();
 
     for i in 0..input.len() {
         for j in i + 1..input.len() {
