f089971b70ac91b6ede15b3c0c7efa919f13f98d
Fredrik Fornwall <fredrik@fornwall.net>
2018-12-05 00:39:29 +0100

    Make clippy happy

--- src/main.rs
+++ src/main.rs
@@ -10,23 +10,16 @@ mod day5;
 fn main() {
     if let Some(day) = env::args().nth(1) {
         if let Some(part) = env::args().nth(2) {
-            let problem_set = get_problem_set(&day);
+            let solver = get_problem_set(&day, &part);
 
             let mut input = String::new();
             std::io::stdin()
                 .read_to_string(&mut input)
                 .expect("Error reading input");
 
-            let solution = match part.as_ref() {
-                "1" => (problem_set.0)(input.as_ref()),
-                "2" => (problem_set.1)(input.as_ref()),
-                _ => {
-                    usage();
-                    "".to_owned()
-                }
-            };
+            let solution = solver(input.as_ref());
 
-            println!("Solution part {}: {}", part, solution);
+            println!("{}", solution);
         } else {
             usage();
         }
@@ -39,16 +32,26 @@ fn usage() {
     println!("Arguments: day part [-]")
 }
 
-fn get_problem_set(day: &str) -> (fn(&str) -> String, fn(&str) -> String) {
-    match day {
-        "1" => (day1::part1, day1::part2),
-        "2" => (day2::part1, day2::part2),
-        "3" => (day3::part1, day3::part2),
-        "4" => (day4::part1, day4::part2),
-        "5" => (day5::part1, day5::part2),
+fn get_problem_set(day: &str, part: &str) -> fn(&str) -> String {
+    struct Solutions(fn(&str) -> String, fn(&str) -> String);
+
+    let parts: Solutions = match day {
+        "1" => Solutions(day1::part1, day1::part2),
+        "2" => Solutions(day2::part1, day2::part2),
+        "3" => Solutions(day3::part1, day3::part2),
+        "4" => Solutions(day4::part1, day4::part2),
+        "5" => Solutions(day5::part1, day5::part2),
         _ => {
-            println!("No such problem set: {}", day);
-            std::process::exit(0);
+            println!("No such day: {}", day);
+            std::process::exit(1);
+        }
+    };
+    match part {
+        "1" => parts.0,
+        "2" => parts.1,
+        _ => {
+            println!("No such part: {}", part);
+            std::process::exit(1);
         }
     }
 }
