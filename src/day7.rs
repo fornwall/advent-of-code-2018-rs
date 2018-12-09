9c287e914e5ff51f0d0d7cd394c7ab047643bada
Fredrik Fornwall <fredrik@fornwall.net>
2018-12-09 03:51:58 +0100

    Day 7 solution

--- src/day7.rs
+++ src/day7.rs
@@ -1,5 +1,7 @@
-use std::collections::{BTreeSet, HashMap, HashSet};
+use std::cmp::Ordering;
+use std::collections::{BTreeSet, BinaryHeap, HashMap, HashSet};
 
+#[derive(Eq)]
 struct Step {
     name: char,
     dependencies: HashSet<char>,
@@ -13,6 +15,24 @@ impl Step {
             dependencies: HashSet::new(),
             needed_by: BTreeSet::new(),
         }
+    }
+}
+
+impl Ord for Step {
+    fn cmp(&self, other: &Step) -> Ordering {
+        other.name.cmp(&self.name)
+    }
+}
+
+impl PartialOrd for Step {
+    fn partial_cmp(&self, other: &Step) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl PartialEq for Step {
+    fn eq(&self, other: &Step) -> bool {
+        self.name == other.name
     }
 }
 
@@ -42,15 +62,15 @@ pub fn part1(input_string: &str) -> String {
     }
 
     // Topological sorting:
-    let mut queue: Vec<&Step> = step_map
+    let mut queue: BinaryHeap<&Step> = BinaryHeap::new();
+    step_map
         .values()
-        .clone()
         .filter(|step| step.dependencies.is_empty())
-        .collect();
+        .for_each(|step| {
+            queue.push(step);
+        });
 
     let mut visited: HashSet<char> = HashSet::new();
-    queue.sort_unstable_by(|a, b| b.name.cmp(&a.name));
-
     let mut result = String::new();
 
     while !queue.is_empty() {
@@ -75,8 +95,116 @@ pub fn part1(input_string: &str) -> String {
     result
 }
 
-pub fn part2(_input_string: &str) -> String {
-    "".to_string()
+#[derive(Eq)]
+struct Work {
+    name: char,
+    done_at_second: i32,
+}
+
+impl Work {
+    fn new(name: char, done_at_second: i32) -> Work {
+        Work {
+            name,
+            done_at_second,
+        }
+    }
+}
+
+impl Ord for Work {
+    fn cmp(&self, other: &Work) -> Ordering {
+        other.done_at_second.cmp(&self.done_at_second)
+    }
+}
+
+impl PartialOrd for Work {
+    fn partial_cmp(&self, other: &Work) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl PartialEq for Work {
+    fn eq(&self, other: &Work) -> bool {
+        self.name == other.name
+    }
+}
+
+pub fn part2_param(input_string: &str, workers: usize, step_duration_base: i32) -> i32 {
+    let mut step_map = HashMap::new();
+    let mut remaining_dependencies: HashMap<char, HashSet<char>> = HashMap::new();
+
+    for line in input_string.lines() {
+        let parts: Vec<&str> = line.split_whitespace().collect();
+        let step_name = parts[7].chars().next().unwrap();
+        let depends_on = parts[1].chars().next().unwrap();
+
+        let step = step_map
+            .entry(step_name)
+            .or_insert_with(|| Step::new(step_name));
+        step.dependencies.insert(depends_on);
+        remaining_dependencies
+            .entry(step_name)
+            .or_insert_with(HashSet::new)
+            .insert(depends_on);
+
+        step_map
+            .entry(depends_on)
+            .or_insert_with(|| Step::new(depends_on))
+            .needed_by
+            .insert(step_name);
+    }
+
+    let mut work_queue: BinaryHeap<Work> = BinaryHeap::new();
+    let mut step_queue: BinaryHeap<&Step> = BinaryHeap::new();
+    step_map
+        .values()
+        .filter(|step| step.dependencies.is_empty())
+        .for_each(|step| {
+            step_queue.push(step);
+        });
+
+    while work_queue.len() < workers && !step_queue.is_empty() {
+        let step = step_queue.pop().unwrap();
+        let done_at_time = step_duration_base + (1 + step.name as i32 - 'A' as i32);
+        work_queue.push(Work::new(step.name, done_at_time));
+    }
+
+    let mut visited: HashSet<char> = HashSet::new();
+    let mut result = String::new();
+
+    let mut latest_work_done_at = 0;
+    while !work_queue.is_empty() {
+        let work_done = work_queue.pop().unwrap();
+        // println!("Step {} done at time {}", work_done.name, work_done.done_at_second);
+        latest_work_done_at = work_done.done_at_second;
+
+        result.push(work_done.name);
+        visited.insert(work_done.name);
+
+        let step = &step_map[&work_done.name];
+
+        for needed_by_name in step.needed_by.iter().rev() {
+            let v = remaining_dependencies.get_mut(&needed_by_name).unwrap();
+            v.remove(&step.name);
+            if v.is_empty() {
+                step_queue.push(&step_map[&needed_by_name]);
+            };
+        }
+
+        while work_queue.len() < workers && !step_queue.is_empty() {
+            let next_step = step_queue.pop().unwrap();
+            let next_step_done_at = work_done.done_at_second
+                + step_duration_base
+                + (1 + next_step.name as i32 - 'A' as i32);
+            // println!("Starting {} to be done at {}", next_step.name, next_step_done_at);
+            work_queue.push(Work::new(next_step.name, next_step_done_at));
+        }
+    }
+
+    latest_work_done_at
+}
+
+pub fn part2(input_string: &str) -> String {
+    part2_param(input_string, 5, 60).to_string()
 }
 
 #[test]
@@ -111,15 +239,27 @@ Step B must be finished before step A can begin."
     );
 
     assert_eq!(
-        "OUGTKDLJVBRMIXSAWCYPEQNHZF",
+        "OUGLTKDJVBRMIXSACWYPEQNHZF",
         part1(include_str!("day7_input.txt"))
     );
 }
 
 #[test]
-#[ignore]
 fn tests_part2() {
-    assert_eq!("", part2(""));
+    assert_eq!(
+        15,
+        part2_param(
+            "Step C must be finished before step A can begin.
+Step C must be finished before step F can begin.
+Step A must be finished before step B can begin.
+Step A must be finished before step D can begin.
+Step B must be finished before step E can begin.
+Step D must be finished before step E can begin.
+Step F must be finished before step E can begin.",
+            2,
+            0
+        )
+    );
 
-    //assert_eq!( "aixwcbzrmdvpsjfgllthdyoqe", part2(include_str!("day2_input.txt")) );
+    assert_eq!("929", part2(include_str!("day7_input.txt")));
 }
